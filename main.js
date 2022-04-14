713ac0624b74c36e64242cc257e6612e57ab44c1
Lucimara Souzah <lucimarasouzah@gmail.com>
2022-04-14 20:13:39 -0300

    end

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// import { API_KEY, BASE_URL, IMG_URL, language } from "./api.js";
+function encontrar() {
+  randomNumber = parseInt(Math.random() * 20);
+  movieDesc.innerHTML = listaFilmes.results[randomNumber].overview;
+  movieImg.src =
+    "https://image.tmdb.org/t/p/w600_and_h900_bestv2" +
+    listaFilmes.results[randomNumber].poster_path;
+  movieTitle.innerHTML = listaFilmes.results[randomNumber].title;
+  if (listaFilmes.results[randomNumber].title == undefined)
+    movieTitle.innerHTML = listaFilmes.results[randomNumber].name;
+}
+encontrar();
